4b46276bd553b6711f539a126faabb5e72a34424
Joshua Skelton <joshua.skelton@gmail.com>
2019-10-03 22:20:08 -0700

    Using SVG viewBox for improved positioning

--- qcli/bsp2svg/converter.py
+++ qcli/bsp2svg/converter.py
@@ -1,15 +1,44 @@
 import svgwrite
 
-from .api import Bsp, subtract, cross
+from .api import Bsp
 
 
-def i(number):
+def simplify_number(number):
+    """Will convert the given number to an integer if number has not fractional
+    part.
+
+    Args:
+        number: The number to convert to an integer.
+
+    Returns:
+        A number.
+    """
     return int(number) if int(number) == number else number
 
 
+def process_vertexes(vertexes):
+    """Converts vertices into points.
+
+    Args:
+        vertexes: A sequence of three-tuples.
+
+    Returns:
+        A sequence of two-tuples.
+    """
+    return [tuple(map(simplify_number, v[:2])) for v in vertexes]
+
+
 def convert(bsp_file, svg_file):
+    """Renders the given bsp file to an svg file.
+
+    Args:
+        bsp_file: A file path to the bsp file to read.
+
+        svg_file: A file path to the svg file to write.
+    """
     bsp_file = Bsp.open(bsp_file)
 
+    # Determine map bounds
     vs = [vertex[:] for model in bsp_file.models for face in model.faces for vertex in face.vertexes]
     xs = [v[0] for v in vs]
     ys = [v[1] for v in vs]
@@ -21,12 +50,11 @@ def convert(bsp_file, svg_file):
 
     width = max_x - min_x
     height = max_y - min_y
-    padding_x = width // 10
-    padding_y = height // 10
+    padding = min(width // 10, height // 10)
 
     dwg = svgwrite.Drawing(
         svg_file,
-        size=(width + padding_x * 2, height + padding_y * 2),
+        viewBox=f'{min_x - padding} {min_y - padding} {width + padding * 2} {height + padding * 2}',
         profile='tiny'
     )
 
@@ -41,15 +69,11 @@ def convert(bsp_file, svg_file):
         drawn_polygons = []
 
         for face in model.faces:
-            vertexes = face.vertexes
-
+            # Ignore perfectly vertical faces
             if face.plane.normal[2] == 0:
                 continue
 
-            # Transform 3D into 2D!
-            points = [tuple(v[:2]) for v in vertexes]
-            points = list(map(lambda x: (i(x[0] - (min_x - padding_x)), i(x[1] - (min_y - padding_y))), points))
-            points = list(map(lambda x: (x[0] * -1 + int(dwg.attribs['width']), x[1]), points))
+            points = process_vertexes(face.vertexes)
 
             # Check if this polygon already exists
             edges = list(zip(points, (points[1:] + [points[0]])))
@@ -61,8 +85,6 @@ def convert(bsp_file, svg_file):
             drawn_polygons.append(polygon)
 
             # Draw the polygon
-            group.add(
-                dwg.polygon(points)
-            )
+            group.add(dwg.polygon(points))
 
     dwg.save()
